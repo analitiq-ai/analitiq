933a7d5259435f9a60f2feeb129183817ae79173
Kirill Andriychuk <kirill@sagedata.net>
2024-07-17 13:03:52 +0200

    Refactor and add functionalities to search_vdb and SQL services
    Redefined default search mode setting at initialization of SearchVdb instances. Added new file check on general.py to raise ValueError for empty files. Implemented new loggers and refactored logging statements for SQL service, splitting execution and chat loggers. The parameters in example_search_docs.py were also updated to locomotive testing setup. Added transformer and torch libraries for added functionality to requirements.txt. Lastly, a new test.py module was added to provide usage example for Analitiq class.

--- libs/cookbooks/query_services.py
+++ libs/cookbooks/query_services.py
@@ -4,12 +4,12 @@ This is an example of how to query Analitiq for information.
 
 from analitiq.main import Analitiq
 
-user_prompt = "Search documentation for number of bikes."
+user_prompt = "Hello"
 
 a = Analitiq()
-services_responses = a.run(user_prompt)
-#print(services_responses)
-for service, response in services_responses.items():
+agent_responses = a.run(user_prompt)
+#print(agent_responses)
+for service, response in agent_responses.items():
     if response:
         response.print_details()
 
