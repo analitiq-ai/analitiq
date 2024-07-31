0ee03df69b9638b979eb343a077ca74f0fbb1486
DaMuBo <daniel910527@googlemail.com>
2024-07-31 21:25:01 +0200

    commit the new code splitter

--- libs/analitiq/utils/document_processor.py
+++ libs/analitiq/utils/document_processor.py
@@ -1,12 +1,20 @@
 import os
 from typing import List, Optional
 from langchain_community.document_loaders import DirectoryLoader, TextLoader
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-
+from langchain_text_splitters import RecursiveCharacterTextSplitter, Language
 
 class DocumentChunkLoader:
     def __init__(self, project_name: str):
         self.project_name = project_name
+
+    def _is_code(self, text: str) -> bool:
+        """Calculate if the given snippet is sql or python code returns true if so."""
+
+    def _text_splitter(self, text: str) -> List[str]:
+        """Split up the texts based on paragraphs, sentences or document sites."""
+
+    def _code_splitter(self, text: str) -> List[str]:
+        """Split up Code snippets based on functions or Selects and Sub Selects."""
 
     def load_and_chunk_documents(self, path: str, extension: Optional[str] = None, chunk_size: int = 2000, chunk_overlap: int = 200):
         """
@@ -35,7 +43,29 @@ class DocumentChunkLoader:
         documents = loader.load()
         doc_lengths = {doc.metadata['source']: len(doc.page_content) for doc in documents}
 
+        code_documents = [doc for doc in documents if self._is_code(doc.page_content)]
+        text_documents = [doc for doc in documents if not self._is_code(doc.page_content)]
+
+        python_splitter = RecursiveCharacterTextSplitter.from_language(
+            language=Language.PYTHON, chunk_size=int(chunk_size), chunk_overlap=int(chunk_overlap))
+        python_chunks = python_splitter.split_documents(code_documents)
+
         doc_splitter = RecursiveCharacterTextSplitter(chunk_size=int(chunk_size), chunk_overlap=int(chunk_overlap))
-        documents_chunks = doc_splitter.split_documents(documents)
+        documents_chunks = doc_splitter.split_documents(text_documents)
+
+        documents_chunks.extend(python_chunks)
 
         return documents_chunks, doc_lengths
+
+if __name__ == "__main__":
+    import pathlib
+
+    ROOT = pathlib.Path(__file__).resolve().parent.parent
+    print(ROOT)
+    EXAMPLES = ROOT / "services/search_vdb/examples/example_test_files"
+
+    chunky = DocumentChunkLoader("my_project")
+    result, doc_leng= chunky.load_and_chunk_documents(path=str(EXAMPLES), extension="*", chunk_size=200, chunk_overlap=10)
+    for r in result:
+        print(r)
+        print("---------------------------------------------")
