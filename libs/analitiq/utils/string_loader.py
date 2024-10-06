6a0089ac150390cdde52ae56a679f8262696910f
DaMuBo <daniel910527@googlemail.com>
2024-10-06 19:58:46 +0200

    added better handling for the document processor created a string loader started working on yaml data splitter

---
+++ libs/analitiq/utils/string_loader.py
@@ -0,0 +1,35 @@
+"""Class for a custom string loader."""
+from typing import AsyncIterator, Iterator, List, Tuple
+
+from langchain_core.document_loaders import BaseLoader
+from langchain_core.documents import Document
+
+
+class StringDocumentLoader(BaseLoader):
+    """An example document loader that reads a list of tuples with texts line by line."""
+
+    def __init__(self, strings: List[Tuple[str, str]]) -> None:
+        """Initialize the loader with a file path.
+
+        This is
+
+        Args:
+        ----
+         strings: List[Tuple[str,str]]
+            Input texts as a list whereas the tuple represents each a document text.
+            The first entryis the text and the second is the filename including file extension.
+
+        """
+        self.strings = strings
+
+    def lazy_load(self) -> Iterator[Document]:  # <-- Does not take any arguments
+        """Load a file line by line in lazy mode.
+
+        When you're implementing lazy load methods, you should use a generator
+        to yield documents one by one.
+        """
+        for text, source in self.strings:
+            yield Document(
+                page_content=text,
+                metadata={"source": source},
+            )
--- libs/tests/unit/utils/test_document_processors.py
+++ libs/tests/unit/utils/test_document_processors.py
@@ -36,3 +36,16 @@ def test_chunk_text():
     text = ""
     expected = []
     assert document_processor.chunk_text(text, chunk_size=20, chunk_overlap=5, token=",") == expected
+
+def test_load_documents_strings():
+    """Test the load documents function."""
+    processor = document_processor.DocumentProcessor("example")
+    testdata = [
+        ("I'm an example text", "text.txt")
+    ]
+
+    docs = processor.load_documents(testdata)
+    assert len(docs) == 1
+    assert docs[0].page_content == testdata[0][0]
+    assert docs[0].metadata.get("source") == testdata[0][1]
+
